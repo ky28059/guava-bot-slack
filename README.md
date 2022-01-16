e9c03f39908ca29ac4d7c8bc2eabb8b6ae0acdb1
Kevin Yu <kevinyu567@gmail.com>
2022-01-15 18:34:05 -0800

    Add sheets API, remove ngrok

--- README.md
+++ README.md
@@ -7,14 +7,29 @@ desired port to run the server on:
 // config.ts
 export const signingSecret = 'legitimate-slack-signing-secret';
 export const token = 'xoxb-also-legitimate-slack-token';
-export const port = 443;
+export const port = 3000;
+
+export const spreadsheetId = 'some-spreadsheet-id';
 ```
 Install dependencies with `npm install` and run `npm start` to run the bot.
-Note that the current version uses `ngrok`, which needs to first be set up [here](https://dashboard.ngrok.com/get-started/setup).
-Also, a new URL will be generated every time the program is run, which the Slack application should be updated with 
-accordingly.
 
 ### Slack
 When creating the Slack application, note that boltjs requires that all URLs are appended with `/slack/events`. If your
-server is running at `https://example.com`, set the event subscription URL and URL for all created slash commands to be 
-`https://example.com/slack/events`.
+server is running at `13.00.255.255` on port `3000`, set the event subscription URL and request URL for all created slash 
+commands to be `http://13.00.255.255:3000/slack/events`.
+
+Guava bot requires the following scopes:
+- `commands` to add and receive slash command interactions
+- `chat:write` to send messages
+- `users:read` to extract `user.real_name` from `command.user_id`
+- `reactions:read` to receive `reaction_add` events
+
+### Google
+To use the sheets API, create a [Google cloud project](https://console.cloud.google.com) and enable the [Google Sheets
+API](https://console.cloud.google.com/marketplace/product/google/sheets.googleapis.com). Create a service account via
+`Create Credentials > Google Sheets API > Application data`. In your service account details page, save your key file 
+(`Keys > Add Key > Create new key > JSON`) as `keys.json`.
+[...]
+
+### Deployment
+[...]
--- bot.ts
+++ bot.ts
@@ -1,5 +1,5 @@
-import { App } from '@slack/bolt';
-import ngrok from 'ngrok';
+import {App, SectionBlock} from '@slack/bolt';
+import {getHours} from './util';
 import { signingSecret, token, port } from './config';
 
 
@@ -8,27 +8,95 @@ const app = new App({
     token
 });
 
-// /help [command]?
-app.command('/help', async ({command, ack, respond}) => {
+type Command = {name: string, pattern: string, desc: string};
+const commands: Command[] = [
+    {name: 'help', pattern: '/help [command]?', desc: 'Sends info about other available commands.'},
+    {name: 'signups', pattern: '/signups', desc: 'Sends a link to the shoptime signup spreadsheet.'},
+    {name: 'hours', pattern: '/hours', desc: 'Sends how many hours you\'ve signed up for this week.'},
+    {name: 'gogurt', pattern: '/gogurt', desc: 'Go GRT!'},
+];
+
+
+// /signups
+// Sends a link to the shoptime signup spreadsheet.
+// TODO: consider adding advanced features like which days have free slots, which week it is, how many hours a person has, etc.
+app.command('/signups', async ({command, ack, respond}) => {
+    await ack();
+    await respond({
+        blocks: [
+            {type: 'header', text: {type: 'plain_text', text: 'GRT Shoptime Signups'}},
+            {type: 'section', text: {type: 'mrkdwn', text: '<https://tinyurl.com/grtBuildSignups2022|Signup Sheet>'}}
+        ]
+    });
+});
+
+// /signup [day] [hours]
+// Signs a user up for the day and time range specified.
+// TODO: implement this
+app.command('/signup', async ({command, client, ack, respond}) => {
     await ack();
     await respond('This command is currently unimplemented. Pester Kevin to complete it!');
-    console.log(`received ${command.text}`);
+
+    const res = await client.users.info({ token, user: command.user_id });
+    console.log(res.user?.real_name);
 });
 
-// /ping
-app.command('/ping', async ({command, ack, respond}) => {
+// /hours
+// Returns the number of hours a user has signed up for in the current week.
+app.command('/hours', async ({command, client, ack, respond}) => {
     await ack();
-    await respond('Pong!');
+
+    // Extract Slack real_name from user ID
+    const {user} = await client.users.info({ token, user: command.user_id });
+    if (!user?.real_name) return;
+
+    const parsed = await getHours(user.real_name);
+    if (!parsed) return respond('An error occurred parsing your name.');
+
+    await respond({
+        blocks: [
+            {type: 'header', text: {type: 'plain_text', text: `Shoptime hours, week of ${parsed.week}`}},
+            {type: 'section', text: {type: 'mrkdwn', text: `You have signed up for ${parsed.hours} hours of shoptime this week.`}}
+        ]
+    });
 });
 
-// reaction_added
+// /gogurt
+// Go GRT!
+app.command('/gogurt', async ({command, ack, respond}) => {
+    await ack();
+    await respond('Go GRT! 👏');
+});
+
+// /help [command]?
+// Sends info about other available commands.
+// TODO: support search by command name
+app.command('/help', async ({command, ack, respond}) => {
+    await ack();
+
+    // Group commands in pairs, map to Slack Block Kit sections
+    const sections: SectionBlock[] = [];
+    for (let i = 0; i < commands.length; i += 2) {
+        const fields: SectionBlock['fields'] = [{type: 'mrkdwn', text: `*${commands[i].pattern}*\n${commands[i].desc}`}];
+        if (i + 1 < commands.length)
+            fields.push({type: 'mrkdwn', text: `*${commands[i + 1].pattern}*\n${commands[i + 1].desc}`});
+        sections.push({type: 'section', fields});
+    }
+
+    await respond({
+        blocks: [
+            {type: 'header', text: {type: 'plain_text', text: 'Commands'}},
+            ...sections
+        ]
+    });
+});
+
+// TODO: log reactions on spreadsheet
 app.event('reaction_added', async ({event}) => {
     console.log(event.reaction);
 });
 
 ;(async () => {
-    const url = await ngrok.connect(port);
     await app.start(port);
-
-    console.log(`Running on ${url}`);
+    console.log(`Started on port ${port}`);
 })();
